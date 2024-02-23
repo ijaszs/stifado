a092484d8f10fc457432a8bb61ba66a1dda4c724
muhammed ijas <ijasbooser@gmail.com>
2024-02-23 22:15:03 +0530

    add static image and style

--- stifado/core/models.py
+++ stifado/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# prodect models
+# class prodect (models.model):
+#     LIVE = 1
+#     DELETE = 0
+#     DELETE_CHOICES = ((LIVE,'live'),(DELETE,'delete'))
+#     title = models.CharField( max_length=255)
+#     price = models.FloatField()
+#     description= models.TextField()
+#     image= models.ImageField(upload_to="/images")
+#     priority =models.IntegerField(default=0)
+#     delete_status= models.IntegerField(choices=DELETE_CHOICES,default='live')
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     upadated_at = models.DateTimeField(auto_now=True)
+
+
+#     def __str__(self) :
+#         return self.title
+    
+
+
+
+
+
+
