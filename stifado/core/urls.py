dad35043de401aaf7d92ace5386b3ac6e99c1106
distroZX <sf874245@gmail.com>
2024-02-26 22:36:53 +0530

    login page created

--- stifado/core/urls.py
+++ stifado/core/urls.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.conf.urls.static import static 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('login', views.login, name='login'),
+
 
 
 
