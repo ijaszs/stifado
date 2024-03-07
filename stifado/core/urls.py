7ae6bc73b1a49746862cc3114cf3e6a172f5d8d5
muhammed ijas <ijasbooser@gmail.com>
2024-03-07 11:25:47 +0530

    login log out updates

--- stifado/core/urls.py
+++ stifado/core/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path,include
-from django.contrib.auth import views as authViews
+from django.contrib.auth import views as auth_views
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static 
 urlpatterns = [
         path('', views.index, name='index'),
-        path('signup/',views.authViews,name='signup'),
+        path('register_user/',views.register_user,name='register'),
+        path('login/',views.login_user,name='login'),
+        path('log_out/',views.logout_user,name='logout_user'),
         path('cart/',views.cart,name='cart'),
         path('prodect_list/',views.prodect_list,name='prodectlist'),
 
--- stifado/stifado/settings.py
+++ stifado/stifado/settings.py
@@ -83,9 +83,7 @@ DATABASES = {
         'PASSWORD': 'muhammedijas123', 
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
-        'OPTIONS': {  
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-    }
+    
 }
 }
 
