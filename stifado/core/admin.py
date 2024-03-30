e8cde200eeba79104dec264d19502d06601812b6
muhammed ijas <ijasbooser@gmail.com>
2024-03-30 20:47:36 +0530

    add register to admin page

--- stifado/core/admin.py
+++ stifado/core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Restaurant, Product,CustomUser
+from .models import Restaurant, Product,CustomUser,Image
 
 admin.site.register(Restaurant)
 admin.site.register(Product)
 admin.site.register(CustomUser)
+admin.site.register(Image)
 
 
 
